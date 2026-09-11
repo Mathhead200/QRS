981dfb601319fac075844950d10d58b9176cbcdc
Christopher D'Angelo <chris@mathhead200.com>
2026-09-11 09:12:12 -0400

    Update QRCode.js

--- QRCode.js
+++ QRCode.js
@@ -126,6 +126,12 @@ export function characterCountBits(version, mode) {
 	throw QRCodeError(`Couldn't determine characterCountBits for verion ${version}, mode ${QRCode._modeToString(mode)}`);
 }
 
+/**
+ * Calculateds the size (in bits) of the encoded data as a function of mode and characterCount.
+ * @param {number} mode A valid QR DataSegment mode
+ * @param {number} characterCount A valid character count (unit depends on mode)
+ * @returns {number} The number of bits for the encoded data
+ */
 export function dataSize(mode, characterCount) {
 	return new Map([
 		[ Segment.NUMERIC,      DataSize.numeric      ],
@@ -133,6 +139,46 @@ export function dataSize(mode, characterCount) {
 		[ Segment.BYTE,         DataSize.bytes        ],
 		[ Segment.KANJI,        DataSize.kanji        ]
 	]).get(mode)(characterCount);
+}
+
+/**
+ * @param {number} eciDesignator 
+ * @returns {number} The number of bits for the given eciDesignator field
+ */
+export function eciDesignatorBits(eciDesignator) {
+	if (eciDesignator <= 0xFF)
+		return 8;
+	if (eciDesignator <= 0xFFFF)
+		return 16;
+	return 24;
+}
+
+/**
+ * Inserts the given number in big-endian (MSB first) order into the given buffer.
+ * @param {number} x An unsigned integer
+ * @param {ArrayLike<number>} buffer An uncompressed bit stream
+ * @param {number} offset The posiion to insert the number
+ * @param {number} bits The explicit size of the insertion in bits.
+ * 	This is needed since some numbers may be 0-padded. (e.g. "3" in 4 bits is 0011)
+ * @returns {number} The new offset/position in buffer where the append ended.
+ */
+function appendNumber(x, buffer, offset, bits) {
+	while (bits > 0)
+		buffer[offset++] = (x >>> --bits) & 0x1;
+	return offset;
+}
+
+/**
+ * Inserts the given buffer (x) into the given output buffer (buffer).
+ * @param {ArrayLike<number>} x An uncompressed bit stream to insert.
+ * @param {ArrayLike<number>} buffer An uncompressed bit stream to be modified.
+ * @param {number} offset The position in buffer to insert.
+ * @returns {number} The new offset/position in buffer where the append ended.
+ */
+function appendBuffer(x, buffer, offset) {
+	for (let i = 0; i < x.length; i++)
+		buffer[offset++] = x[i];
+	return offset;
 }
 
 /**
@@ -160,65 +206,140 @@ export class Segment {
 	constructor(qrCode, mode) {
 		this.qrCode = qrCode;  // the parent QRCode this segment will be attached to
 		this.mode = validateMode(mode);
+		this.size = 4;  // override in subclasses -- size of cannonical bit stream
 	}
 
 	appendMode(buffer, offset = 0) {
-		// TODO: append mode to bit stream buffer starting at index 0
+		appendNumber(this.mode, buffer, offset, 4);
 	}
 
-	bitStream() {
-		throw new Error("Abstract method");
-	}
+	// abstract:
+	/**
+	 * Convert this Segment into its cannonical bit stream (uncompressed).
+	 * @returns {ArrayLike<number>}
+	 */
+	bitStream() { throw new Error("Abstract method"); }
 }
 
 export class DataSegment extends Segment {
+	/**
+	 * @param {QRCode} qrCode 
+	 * @param {number} mode 
+	 * @param {number} characterCount 
+	 * @param {ArrayLike<number>} data An (uncompressed) bit stream containing 0 or 1 elements only
+	 */
 	constructor(qrCode, mode, characterCount, data) {
 		super(qrCode, mode);
 		this.characterCount = validateCharacterCount(characterCount, qrCode.version, mode);
-		this.data = validateData(data, mode, characterCount);  // {ArrayList<number>} an (uncompressed) bit stream containing 0 or 1 elements only
+		this.data = validateData(data, mode, characterCount);
+		this.size = 4 + characterCountBits(this.qrCode.version, this.mode) + this.data.length;
 	}
 
+	/**
+	 * @param {ArrayLike<number>} buffer 
+	 * @param {number} offset
+	 * @returns {number} The new offset/position in buffer where the append ended
+	 */
 	appendCharacterCount(buffer, offset = 4) {
-		// TODO: ...
+		return appendNumber(this.characterCount, buffer, offset, characterCountBits(this.qrCode.version, this.mode));
 	}
 
+	/**
+	 * @param {ArrayLike<number>} buffer 
+	 * @param {number} offset
+	 * @returns {number} The new offset/position in buffer where the append ended
+	 */
 	appendData(buffer, offset) {
-		// TODO: ...
+		return appendBuffer(this.data, buffer, offset);
 	}
 
+	/** @override */
 	bitStream() {
-		let ccBits = characterCountBits(this.qrCode.version, this.mode);
-		let buffer = new Uint8Array(4 + ccBits + this.data.length);
-		this.appendMode(buffer, 0);
-		this.appendCharacterCount(buffer, 4);
-		this.appendData(buffer, 4 + ccBits);
+		let buffer = new Uint8Array(this.size);
+		let pos = 0;
+		pos = this.appendMode(buffer, pos);
+		pos = this.appendCharacterCount(buffer, pos);
+		pos = this.appendData(buffer, pos);
 		return buffer;
 	}
 }
 
 export class ECISegment extends Segment {
+	/**
+	 * @param {QRCode} qrCode 
+	 * @param {number} eciDesignator 
+	 */
 	constructor(qrCode, eciDesignator) {
 		super(qrCode, Segment.ECI);
 		this.eciDesignator = validateEciDesignator(eciDesignator);
+		this.size = 4 + eciDesignatorBits(this.eciDesignator);
 	}
 
-	// TODO: ...
+	/**
+	 * @param {ArrayLike<number>} buffer 
+	 * @param {number} offset 
+	 * @returns The new offset/position in buffer where the append ended
+	 */
+	appendECIDesignator(buffer, offset = 4) {
+		return appendNumber(this.eciDesignator, buffer, offset, eciDesignatorBits(this.eciDesignator));
+	}
+
+	/** @override */
+	bitStream() {
+		let buffer = new Uint8Array(this.size);
+		let pos = 0;
+		pos = this.appendMode(buffer, pos);
+		pos = this.appendECIDesignator(buffer, pos);
+		return buffer;
+	}
 }
 
 export class NullSegment extends Segment {
-	constructor(qrCode) {
+	/**
+	 * @param {QRCode} qrCode 
+	 */
+	constructor(qrCode, bits = 4) {
 		super(qrCode, Segment.NULL);
+		this.size = bits;  // since the NullSegment may be truncated or missing if there is no padding.
 	}
 
-	// TODO: ...
+	/** @override */
+	bitStream() {
+		// The null terminator (end of message) segment's mode indicator is all 0's
+		// which is the default for Uint8Array
+		return new Uint8Array(this.size);
+	}
 }
 
 export class QRCode {
-	constructor(version) {
+	/**
+	 * @param {number} QRCode version (i.e. grid size)
+	 * @param {ArrayLike<Segment>} segments The segment(s) in this QRCode.
+	 *	A NullSegment is required at the end if there is padding.
+	 * @param {ArrayLike<number>} padding The QR code spec. specifies what these padding bits should be exactly,
+	 *	however, this program supports non-standard padding as well. Non-standard padding bits will not be rejected.
+	 *	Only the size of the buffers will be validated.
+	 */
+	constructor(version, segments, padding) {
 		this.version = validateVersion(version);
-		this.segments = [];  // {ArrayList<Segment>}
+
+		/**
+		 * @type {ArrayList<Segment>}
+		 */
+		this.segments = [];
+
+		/**
+		 * @type {ArrayLike<number>}
+		 */
 		this.padding = [];  // {ArrayList<number>} an (uncompressed) bit stream containing 0 or 1 elements only
 	}
 
-	// TODO: ...
+	/**
+	 * Convert this QRCode into its cannonical bit stream (uncompressed).
+	 * @returns {ArrayLike<number>}
+	 */
+	bitStream() {
+		let bits = 13 * 8;  // TODO: (stub) determine toal number of bits
+		for 
+	}
 }
